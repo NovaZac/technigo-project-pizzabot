69a61bdc54f50dc4e7f6440b010cd5980efbe6af
Nova Zackrisson <novazackrisson@airsomtlhornova.home>
2024-08-21 19:14:41 +0200

    Step 3

--- code/script.js
+++ code/script.js
@@ -12,28 +12,38 @@ let username = prompt(`Please enter your name!`)
 }
 
 // Step 2 - Food choice
-// Your code goes here
+// Step 3 - Subtype choice
 
 let chooseFood = prompt(
-   `What are you craving today?
-   Enter a number:
-   1 - Pizza
-   2 - Pasta
-   3 - Salad`)
+  `What are you craving today?
+  Enter a number:
+  1 - Pizza
+  2 - Pasta
+  3 - Salad`)
 
-   if (chooseFood == 1) {
-    alert(`Pizza, amazing choice!`)
+  if (chooseFood == 1) {
+   alert(`Pizza, amazing choice!`)
+    prompt(`Which pizza appeals to you today?
+      1 - Margahrita
+      2 - Caprina
+      3 - Tricolore`)
    } else if ( chooseFood == 2){
     alert(`Pasta! You have a really good taste!`)
+      prompt(`Lovely, which pasta would you like?
+        1 - Alfredo
+        2 - Bolognese
+        3 - Pomodoro`)
    } else if (chooseFood == 3){
     alert(`Our salad will not disappiont you!`)
+      prompt(`Which salad do you prefer?
+        1 - Caprese
+        2 - Gamberetti
+        3 - Pollo`)
    } else {
     alert(`Invalid, you must choose a number between 1-3`)
    }
 
-  
-// Step 3 - Subtype choice
-// Your code goes here
+
 
 // Step 4 - Age
 // Your code goes here
